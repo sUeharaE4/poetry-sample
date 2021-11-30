9154117ae856ecf119dd898608ccaceb3e9ca8fd
sUeharaE4 <funa1hasi2hasi@gmail.com>
2021-11-30 18:26:59 +0900

    Fix: document source path

--- build/docs/source/conf.py
+++ build/docs/source/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../src'))
+sys.path.insert(0, os.path.abspath('../../../'))
 
 # -- Project information -----------------------------------------------------
 
